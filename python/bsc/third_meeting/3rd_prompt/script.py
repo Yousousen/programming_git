2f8ae78b156dbd657cfe3efc168a6af5fcc6f11e
Kaydo <kaydo1@live.nl>
2023-05-02 11:42:19 +0200

    Having trouble installing my specific version of numpy without it building from source. While I'm at it, I discover that only one hidden layer is tried by Ray Tune, and I modify the code to try up to three hidden layers.

--- python/bsc/third_meeting/3rd_prompt/script.py
+++ python/bsc/third_meeting/3rd_prompt/script.py
@@ -3,7 +3,7 @@
 
 # # Experimenting to optimize for h
 
-# In[1]:
+# In[35]:
 
 
 # Import the necessary modules
@@ -44,7 +44,7 @@ def h(x1, x2, x3):
     return x3 * x1**(x2)
 
 
-# In[2]:
+# In[36]:
 
 
 # Define the range and step size for the input variables
@@ -80,7 +80,7 @@ Y_g_train, Y_g_test = torch.utils.data.random_split(Y_g, [train_size, test_size]
 Y_h_train, Y_h_test = torch.utils.data.random_split(Y_h, [train_size, test_size])
 
 
-# In[3]:
+# In[37]:
 
 
 # Let us have a variable number of hidden layers.
@@ -107,8 +107,7 @@ def create_network(input_size, output_size, hidden_sizes, activations, output_ac
     return model
 
 
-
-# In[4]:
+# In[38]:
 
 
 # Define a function to train a neural network with given hyperparameters and data
@@ -166,7 +165,7 @@ def train_network(model, optimizer, loss_fn, batch_size, epochs,
         return train_losses
 
 
-# In[5]:
+# In[39]:
 
 
 # Define a function to plot the losses during training
@@ -189,7 +188,7 @@ def plot_losses(train_losses, test_losses=None, function_name=None, hyperparamet
     plt.show()
 
 
-# In[6]:
+# In[40]:
 
 
 # Define a function to plot the predictions versus the true values
@@ -212,10 +211,24 @@ def plot_predictions(model, X, Y_true, function_name, hyperparameters=""):
     plt.show()
 
 
-# In[7]:
+# In[41]:
 
 
-get_ipython().run_cell_magic('script', 'echo skipping', '\n# Define a list of functions to be approximated\nfunctions = [f, g, h]\n# Define a list of function names for printing and plotting purposes\nfunction_names = ["f", "g", "h"]\n# Define a list of output tensors for each function\noutputs = [Y_f, Y_g, Y_h]\n# Define a list of output tensors for each function for train and test sets\noutputs_train = [Y_f_train, Y_g_train, Y_h_train]\noutputs_test = [Y_f_test, Y_g_test, Y_h_test]\n\n# Loop over each function to be approximated\nfor i in range(len(functions)):\n    # Print the function name\n    print(f"Approximating function {function_names[i]}")\n    # Create a neural network with given hyperparameters\n    input_size = 3 # The number of input variables (x1, x2, x3)\n    output_size = 1 # The number of output variables (y)\n    # Create a network with 3 hidden layers and ReLU activations, and an optional output activation\n    hidden_sizes = [64, 128, 256, 512]\n    activations = [nn.ELU, nn.ELU, nn.ELU, nn.ELU]\n\n\n    output_activation = None\n    model = create_network(input_size, output_size,\n                        hidden_sizes, activations, output_activation=output_activation)\n\n    # Create an instance of VariableNetwork by passing the model\n    network = VariableNetwork(model)\n\n    # Create an optimizer with given hyperparameters\n    optimizer = optim.Adam(network.parameters(), lr=0.001)\n\n    # Create a loss function with given hyperparameters\n    loss_fn = nn.MSELoss()\n    # Train the network with given hyperparameters and data\n    batch_size = 64 # The number of samples in each batch\n    epochs = 100 # The number of times to loop over the whole dataset\n    # Create a string representation of the hyperparameters\n    hyperparameters_str = f"hidden_sizes_{hidden_sizes}_activations_{[act.__name__ for act in activations]}_optimizer_{optimizer.__class__.__name__}_lr_{optimizer.param_groups[0][\'lr\']}_batch_size_{batch_size}_epochs_{epochs}"\n    if output_activation:\n        hyperparameters_str += f"_output_activation_{output_activation.__name__}"\n\n    if output_activation:\n        hyperparameters_str += f"_output_activation_{output_activation.__name__}"\n\n    train_losses, test_losses = train_network(network, optimizer, loss_fn,\n                                            batch_size, epochs,\n                                            X_train.dataset, outputs_train[i].dataset,\n                                            X_test.dataset, outputs_test[i].dataset)\n    plot_losses(train_losses, test_losses, function_names[i], hyperparameters=hyperparameters_str)\n    plot_predictions(network, X, outputs[i], function_names[i], hyperparameters=hyperparameters_str)\n\n    # Save the network with hyperparameters in the file name\n    torch.save(network, f"network_{function_names[i]}_{hyperparameters_str}.pt")\n')
+# Define a list of functions to be approximated
+functions = [f, g, h]
+# Define a list of function names for printing and plotting purposes
+function_names = ["f", "g", "h"]
+# Define a list of output tensors for each function
+outputs = [Y_f, Y_g, Y_h]
+# Define a list of output tensors for each function for train and test sets
+outputs_train = [Y_f_train, Y_g_train, Y_h_train]
+outputs_test = [Y_f_test, Y_g_test, Y_h_test]
+
+
+# In[42]:
+
+
+get_ipython().run_cell_magic('script', 'echo skipping', '\n# Loop over each function to be approximated\nfor i in range(len(functions)):\n    # Print the function name\n    print(f"Approximating function {function_names[i]}")\n    # Create a neural network with given hyperparameters\n    input_size = 3 # The number of input variables (x1, x2, x3)\n    output_size = 1 # The number of output variables (y)\n    # Create a network with 3 hidden layers and ReLU activations, and an optional output activation\n    hidden_sizes = [64, 128, 256, 512]\n    activations = [nn.ELU, nn.ELU, nn.ELU, nn.ELU]\n\n\n    output_activation = None\n    model = create_network(input_size, output_size,\n                        hidden_sizes, activations, output_activation=output_activation)\n\n    # Create an instance of VariableNetwork by passing the model\n    network = VariableNetwork(model)\n\n    # Create an optimizer with given hyperparameters\n    optimizer = optim.Adam(network.parameters(), lr=0.001)\n\n    # Create a loss function with given hyperparameters\n    loss_fn = nn.MSELoss()\n    # Train the network with given hyperparameters and data\n    batch_size = 64 # The number of samples in each batch\n    epochs = 100 # The number of times to loop over the whole dataset\n    # Create a string representation of the hyperparameters\n    hyperparameters_str = f"hidden_sizes_{hidden_sizes}_activations_{[act.__name__ for act in activations]}_optimizer_{optimizer.__class__.__name__}_lr_{optimizer.param_groups[0][\'lr\']}_batch_size_{batch_size}_epochs_{epochs}"\n    if output_activation:\n        hyperparameters_str += f"_output_activation_{output_activation.__name__}"\n\n    if output_activation:\n        hyperparameters_str += f"_output_activation_{output_activation.__name__}"\n\n    train_losses, test_losses = train_network(network, optimizer, loss_fn,\n                                            batch_size, epochs,\n                                            X_train.dataset, outputs_train[i].dataset,\n                                            X_test.dataset, outputs_test[i].dataset)\n    plot_losses(train_losses, test_losses, function_names[i], hyperparameters=hyperparameters_str)\n    plot_predictions(network, X, outputs[i], function_names[i], hyperparameters=hyperparameters_str)\n\n    # Save the network with hyperparameters in the file name\n    torch.save(network, f"network_{function_names[i]}_{hyperparameters_str}.pt")')
 
 
 # # Ray Tune
@@ -223,7 +236,7 @@ get_ipython().run_cell_magic('script', 'echo skipping', '\n# Define a list of fu
 # I will now implement Ray Tune to find good parameters for our network.
 # I had asked C too to implement more different activation functions, upon which he modified the `create_network` function too.
 
-# In[ ]:
+# In[44]:
 
 
 import multiprocessing
@@ -231,15 +244,17 @@ import multiprocessing
 num_cpus = multiprocessing.cpu_count()
 print(f"Number of CPUs: {num_cpus}")
 
+input_size = 3  # The number of input variables (x1, x2, x3)
+output_size = 1  # The number of output variables (y)
 
-# In[ ]:
+
+# In[45]:
 
 
 import ray
 from ray import tune
 from ray.tune.schedulers import ASHAScheduler
-from ray.tune.search.skopt import SkOptSearch
-from skopt.space import Real, Integer, Categorical
+from ray.tune.suggest.skopt import SkOptSearch
 
 # Create a function to create a neural network with given hyperparameters
 def create_network(input_size, output_size, hidden_sizes, activation_classes, output_activation_class=None):
@@ -282,20 +297,24 @@ def tune_network(config):
 # In[ ]:
 
 
-# Create a search space
+from skopt.space import Real, Integer, Categorical
+
+# Define the search space for SkOpt
 search_space = {
-    "hidden_sizes": tune.grid_search([[32, 64, 128], [64, 128, 256], [128, 256, 512]]),
-    "activation_classes": tune.grid_search([
-        ["ReLU", "ReLU", "ReLU"],
-        ["LeakyReLU", "LeakyReLU", "LeakyReLU"],
-        ["ELU", "ELU", "ELU"],
-        ["Tanh", "Tanh", "Tanh"]
-    ]),
-    "output_activation_class": tune.choice([None, "Sigmoid", "Tanh"]),
-    "lr": Real(1e-4, 1e-2, prior="log-uniform"),
-    "batch_size": Integer(16, 128),
-    "epochs": Integer(50, 200)
+    "hidden_sizes": Integer(32, 1024),
+    "activation_classes": Categorical(["ReLU", "ELU", "LeakyReLU", "Tanh", "Sigmoid"]),
+    "output_activation_class": Categorical([None, "ReLU", "ELU", "LeakyReLU", "Tanh", "Sigmoid"]),
+    "lr": Real(1e-4, 1e-2, "log-uniform"),
+    "batch_size": Integer(32, 256),
+    "epochs": Integer(10, 200),
 }
+
+# Initialize SkOpt search algorithm
+skopt_search = SkOptSearch(space=search_space, metric="test_loss", mode="min")
+
+
+# In[ ]:
+
 
 # Set up the scheduler, searcher, and resources
 asha_scheduler = ASHAScheduler(
@@ -306,40 +325,42 @@ asha_scheduler = ASHAScheduler(
     reduction_factor=2
 )
 
-skopt_search = SkOptSearch(space=search_space, metric="test_loss", mode="min")
-
 resources_per_trial = {"cpu": num_cpus, "gpu": 0}
 
-# Start the tuning process
-analysis = tune.run(
-    tune_network,
-    search_alg=skopt_search,
-    scheduler=asha_scheduler,
-    num_samples=50,
-    resources_per_trial=resources_per_trial,
-    config=search_space,
-    name="tune_network"
-)
+for i in range(len(functions)):
+    # Print the function name
+    print(f"Approximating function {function_names[i]}")
 
-# Get the best set of hyperparameters
-best_trial = analysis.get_best_trial("test_loss", "min", "last")
-best_config = best_trial.config
-print(f"Best configuration: {best_config}")
+    # Start the tuning process
+    analysis = tune.run(
+        tune_network,
+        search_alg=skopt_search,
+        scheduler=asha_scheduler,
+        num_samples=50,
+        resources_per_trial=resources_per_trial,
+        config=search_space,
+        name=f"tune_network_{function_names[i]}"
+    )
 
-# Train the network with the best hyperparameters
-best_activation_classes = [getattr(nn, act_class_name) for act_class_name in best_config["activation_classes"]]
-best_hidden_sizes = best_config["hidden_sizes"]
-best_output_activation_class = getattr(nn, best_config["output_activation_class"]) if best_config["output_activation_class"] else None
-best_model = create_network(input_size, output_size, best_hidden_sizes, best_activation_classes, output_activation_class=best_output_activation_class)
-best_network = VariableNetwork(best_model)
-best_optimizer = optim.Adam(best_network.parameters(), lr=best_config["lr"])
-best_loss_fn = nn.MSELoss()
+    # Get the best set of hyperparameters
+    best_trial = analysis.get_best_trial("test_loss", "min", "last")
+    best_config = best_trial.config
+    print(f"Best configuration: {best_config}")
 
-best_train_losses, best_test_losses = train_network(best_network, best_optimizer, best_loss_fn,
-                                                        best_config["batch_size"], best_config["epochs"],
-                                                        X_train.dataset, Y_f_train.dataset,
-                                                        X_test.dataset, Y_f_test.dataset)
+    # Train the network with the best hyperparameters
+    best_activation_classes = [getattr(nn, act_class_name) for act_class_name in best_config["activation_classes"]]
+    best_hidden_sizes = best_config["hidden_sizes"]
+    best_output_activation_class = getattr(nn, best_config["output_activation_class"]) if best_config["output_activation_class"] else None
+    best_model = create_network(input_size, output_size, best_hidden_sizes, best_activation_classes, output_activation_class=best_output_activation_class)
+    best_network = VariableNetwork(best_model)
+    best_optimizer = optim.Adam(best_network.parameters(), lr=best_config["lr"])
+    best_loss_fn = nn.MSELoss()
 
-# Print the test loss for the best model
-print(f"Test loss for the best model: {best_test_losses[-1]}")
+    best_train_losses, best_test_losses = train_network(best_network, best_optimizer, best_loss_fn,
+                                                         best_config["batch_size"], best_config["epochs"],
+                                                         X_train.dataset, outputs_train[i].dataset,
+                                                         X_test.dataset, outputs_test[i].dataset)
+
+    # Print the test loss for the best model
+    print(f"Test loss for the best model: {best_test_losses[-1]}")
 
