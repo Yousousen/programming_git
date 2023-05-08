d3248600a8d6df1abf3c3521a17e125c8e8e0365
Yousousen <kaydo1@live.nl>
2023-05-08 21:06:55 +0200

    Attempting the c++ to work

--- python/bsc/con2prim_restoration/cpp/pt3.cpp
+++ python/bsc/con2prim_restoration/cpp/pt3.cpp
@@ -1,41 +1,43 @@
 #include <torch/script.h>
+#include <iostream>
 
-// Checking if CUDA is available
-bool cuda_available = torch::cuda::is_available();
+int main() {
+  // Declaring a variable to store the model
+  torch::jit::script::Module model;
 
-// Setting the device accordingly
-at::Device device = cuda_available ? at::kCUDA : at::kCPU;
+  // Loading the model from the file "net.pt" using the torch::jit::load function
+  try {
+    // Deserialize the ScriptModule from a file using torch::jit::load().
+    model = torch::jit::load("net.pt");
+  }
+  catch (const c10::Error& e) {
+    std::cerr << "error loading the model\n";
+    return -1;
+  }
 
-// Loading the model from the file
-std::shared_ptr<torch::jit::script::Module> module = torch::jit::load("../model.pt");
+  // Printing a message to indicate successful loading
+  std::cout << "Model loaded successfully\n";
 
-// Converting the input data to a tensor
-torch::Tensor input = torch::tensor({{ 4.9706,  0.3188,  8.5965},
-                                     { 7.9518,  5.3166,  4.3170},
-                                     { 5.8308, 14.7490, 14.6609},
-                                     { 4.6200,  0.0829,  2.7031},
-                                     { 1.5898,  3.1772,  3.5144},
-                                     { 0.1266,  0.1761,  0.1656},
-                                     { 8.0950, 10.3077, 15.3977},
-                                     { 4.2992,  8.2627,  6.6898},
-                                     {11.2275, 10.1282,  7.7118},
-                                     { 2.2244,  0.0301,  3.4477},
-                                     { 3.6509,  2.7749,  2.8721},
-                                     { 6.7411, 18.4558, 17.8222},
-                                     { 9.5191, 24.9498, 24.5091},
-                                     { 5.3015,  4.9007,  3.7724},
-                                     { 1.4423,  1.3792,  1.0336},
-                                     { 3.4728,  2.6612,  2.0619},
-                                     { 5.1982,  1.5509,  1.6710},
-                                     { 7.4406,   .4610 ,   .5647 },
-                                     {   .8908 ,   .3629 ,   .9034 },
-                                     {   .3611 ,   .4911 ,   .3751 }});
+  // Checking if CUDA is available
+  //bool cuda_available = torch::cuda::is_available();
+
+  // Setting the device accordingly
+  //at::Device device = cuda_available ? at::kCUDA : at::kCPU;
+  at::Device device = at::kCPU;
 
 
-// Specifying the device for the module and the input tensor
-module->to(device);
-input = input.to(device);
+  // Converting the input data from python to C++ using torch::from_blob function
+  float input_data[] = {12.0043, 32.6378, 31.5031};
+  auto input_tensor = torch::from_blob(input_data, {1, 3});
 
-// Evaluating the model on the input tensor
-torch::Tensor output = module->forward({input}).toTensor();
+  // Moving the input tensor to the same device as the model
+  input_tensor = input_tensor.to(model.device());
 
+  // Evaluating the model on the input tensor using the forward method
+  auto output_tensor = model.forward({input_tensor}).toTensor();
+
+  // Printing the output tensor
+  std::cout << "Output: " << output_tensor << "\n";
+
+  return 0;
+}
