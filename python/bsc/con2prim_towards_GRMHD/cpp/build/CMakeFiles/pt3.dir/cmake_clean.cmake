8cee28836a09421cda2d0f64c1604ec88afb332f
Yousousen <kaydo1@live.nl>
2023-05-16 14:37:02 +0200

    Created directory `con2prim_towards_GRMHD`. I went to implement the steps of adding vy and vz myself up to the plot of the input data before normalization, which I didn't finish. I now decide to let AI do the task for me instead.

---
+++ python/bsc/con2prim_towards_GRMHD/cpp/build/CMakeFiles/pt3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pt3.dir/pt3.cpp.o"
+  "CMakeFiles/pt3.dir/pt3.cpp.o.d"
+  "pt3"
+  "pt3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pt3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
