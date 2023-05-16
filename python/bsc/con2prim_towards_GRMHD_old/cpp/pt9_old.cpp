cbc0e3298dabc14916718c7ff2a9ab5cd4ce0b46
Yousousen <kaydo1@live.nl>
2023-05-16 14:37:18 +0200

    Added new directory for the AI version of adding vx, vy, vz

---
+++ python/bsc/con2prim_towards_GRMHD_old/cpp/pt9_old.cpp
@@ -0,0 +1,49 @@
+#include <torch/script.h>
+#include <iostream>
+
+int main() {
+  // Set the path to the net.pt file.
+  char path_to_model[] = "../../net.pt";
+
+  // Declaring a variable to store the model
+  torch::jit::script::Module model;
+
+  // Loading the model from the file "net.pt" using the torch::jit::load function
+  try {
+    // Deserialize the ScriptModule from a file using torch::jit::load().
+    model = torch::jit::load(path_to_model);
+  }
+  catch (const c10::Error& e) {
+    std::cerr << "error loading the model, did you correctly set the path to the net.pt file?\n";
+    return -1;
+  }
+
+  // Printing a message to indicate successful loading
+  std::cout << "Model loaded successfully\n";
+
+  // Checking if CUDA is available
+  //bool cuda_available = torch::cuda::is_available();
+
+  // Setting the device accordingly
+  //at::Device device = cuda_available ? at::kCUDA : at::kCPU;
+  //at::Device device = at::kCPU;
+
+  // Moving the model to the same device as the input tensor
+  model.to(at::kCPU);
+
+
+  // Converting the input data from python to C++ using torch::from_blob function
+  float input_data[] = {4.6177, 0.3018, 2.3998};
+  auto input_tensor = torch::from_blob(input_data, {1, 3});
+
+  // Moving the input tensor to the same device as the model
+  input_tensor = input_tensor.to(at::kCPU);
+
+  // Evaluating the model on the input tensor using the forward method
+  auto output_tensor = model.forward({input_tensor}).toTensor();
+
+  // Printing the output tensor
+  std::cout << "Output: " << output_tensor << "\n";
+
+  return 0;
+}
