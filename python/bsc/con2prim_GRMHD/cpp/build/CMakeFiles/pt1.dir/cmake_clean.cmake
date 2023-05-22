139a5141234bda2bb8d8772ef9ed7c23edf976c4
Yousousen <kaydo1@live.nl>
2023-05-22 09:25:36 +0200

    Confirmed correct operation of the c++ code with the eight input parameters version of the code.

---
+++ python/bsc/con2prim_GRMHD/cpp/build/CMakeFiles/pt1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pt1.dir/pt1.cpp.o"
+  "CMakeFiles/pt1.dir/pt1.cpp.o.d"
+  "pt1"
+  "pt1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pt1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
